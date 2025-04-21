92fc9eabeb9f4ee7843a18e7eff1c647ec5a68ac
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-21 12:43:44 -0600

    Combine the top 3 contributors and sort

--- top_three.py
+++ top_three.py
@@ -19,7 +19,7 @@ def get_url(url):
 def get_all(response):
     # Create soup to find all people
     soup = BeautifulSoup(response.text, 'lxml')
-    find_people = soup.find_all('dd')
+    find_people = soup.find_all('form', class_='authsearch')
     # Create a dictionary to store all people
     all_people = dict()
 
@@ -27,6 +27,7 @@ def get_all(response):
     for people in find_people:
         for person in people.find_all('a'):
             name = person.text.strip()
+
             # Add to dictionary if name doesn't exist
             if name not in all_people:
                 all_people[name] = 1
@@ -36,10 +37,46 @@ def get_all(response):
 
     return all_people
 
+def get_top_three(all_2022, all_2023, all_2024):
+    all_total = dict()
+    # Combine all years into one dictionary
+    for person, total in all_2022.items():
+        if person not in all_total:
+            all_total[person] = total
+        else:
+            all_total[person] += total
+
+    for person, total in all_2023.items():
+        if person not in all_total:
+            all_total[person] = total
+        else:
+            all_total[person] += total
+    
+    for person, total in all_2024.items():
+        if person not in all_total:
+            all_total[person] = total
+        else:
+            all_total[person] += total
+
+    '''
+    Sort the dictionary by value and get the top three
+    Use lambda function to sort by value in descending order
+    x[1] gets the value to sort by, the total contributions of each person
+    Return the top three people with the most contributions
+    Reverse as it sorts in ascending order by default
+    Use [:3] to get the top three people
+    '''
+
+    top_three = sorted(all_total.items(), key=lambda x: x[1], reverse=True)[:3]
+    return top_three
+
 def main():
     # Get all people from each year
     all_2022 = get_url(year_2022)
     all_2023 = get_url(year_2023)
     all_2024 = get_url(year_2024)
 
+    # Get the top three people with the most contributions
+    top_three = get_top_three(all_2022, all_2023, all_2024)
+    
 main()
