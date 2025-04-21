29e99b2bee75692050c5b675b0cf0492ddfa6d04
Jacob Yepez <jacobyepez04@gmail.com>
2025-04-21 12:24:13 -0600

    Added get all people for each year and each url

--- top_three.py
+++ top_three.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Set all urls
+year_2022 = 'https://openaccess.thecvf.com/CVPR2022?day=all'
+year_2023 = 'https://openaccess.thecvf.com/CVPR2023?day=all'
+year_2024 = 'https://openaccess.thecvf.com/CVPR2024?day=all'
+
 def get_url(url):
     response = requests.get(url)
     # Check response if the response is successful
@@ -18,3 +23,23 @@ def get_all(response):
     # Create a dictionary to store all people
     all_people = dict()
 
+    # Loop through all people and add them to the dictionary
+    for people in find_people:
+        for person in people.find_all('a'):
+            name = person.text.strip()
+            # Add to dictionary if name doesn't exist
+            if name not in all_people:
+                all_people[name] = 1
+            # If name exists, increment the count
+            else:
+                all_people[name] += 1
+
+    return all_people
+
+def main():
+    # Get all people from each year
+    all_2022 = get_url(year_2022)
+    all_2023 = get_url(year_2023)
+    all_2024 = get_url(year_2024)
+
+main()
